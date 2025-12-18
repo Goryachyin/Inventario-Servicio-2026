e2611eb9e734ee337facda88726eeab759ae366f
Goryachyin <cuauhtemocmojica@gmail.com>
2025-12-17 18:37:18 -0600

    Plantilla de inicio, eslint Standard

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadURL('https://www.youtube.com/')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
